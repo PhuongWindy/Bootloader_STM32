77f9cbf11689b9ccd08a559e595281c82cdb400f
PhuongWindy <phuongnguyenbkvp@gmail.com>
2024-08-11 22:51:24 +0700

    App update FW UART part

--- App/Update_FW/mainwindow.h
+++ App/Update_FW/mainwindow.h
@@ -6,6 +6,14 @@
 #include <QTextEdit>
 #include <QDebug>
 #include <QString>
+#include <QMessageBox>
+#include <QByteArray>
+#include <QPlainTextEdit>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QIODevice>
+#include <QTimer>
+#include <QPalette>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -20,13 +28,42 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    enum color {
+        red,
+        green
+    };
+
+    enum Protocol {
+        UART,
+        TCP,
+        UDP
+    };
+    QByteArray fileData;
+    uint8_t CRC[2];
 
 private slots:
-    void on_bttFile_clicked();
+
+    void BttFile_clicked();
+
+    void BttUart_clicked();
+
+    void BttTcp_clicked();
+
+    void BttUdp_clicked();
+
+    void BttFlash_clicked();
 
 private:
     Ui::MainWindow *ui;
-    QTextEdit *plTextFile;
-    QTextEdit *plTextCode;
+    QPlainTextEdit *pTdirection;
+    QSerialPort  *comPort;
+
+    void loadPort();
+    void setupPortUpdater();
+    void setColorButtonUart(uint8_t Color);
+    void uartSendData();
+    void tcpSendDada();
+    void udpSendData();
+    uint16_t calculateCRC16(uint8_t *data);
 };
 #endif // MAINWINDOW_H
